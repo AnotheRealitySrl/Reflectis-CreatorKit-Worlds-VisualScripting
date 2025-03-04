9d4d41a1ed949389371b7fcffd1d7da5a615381c
Raffaele Tolusso <raffaele.tolusso@gmail.com>
2025-03-04 15:20:27 +0100

    added upgrade window

---
+++ Editor/Scripts/CreatorKitUpgradeWindow.cs
@@ -0,0 +1,183 @@
+using Reflectis.CreatorKit.Worlds.Placeholders;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+
+using UnityEngine;
+
+namespace Reflectis.CreatorKit.Worlds.CoreEditor
+{
+    public class CreatorKitUpgradeWindow : EditorWindow
+    {
+        private readonly List<KeyValuePair<string, string>> stringsToReplace = new()
+        {
+            new KeyValuePair<string, string>("Reflectis.PLG.Graphs", "Reflectis.SDK.Graphs"),
+            new KeyValuePair<string, string>("Reflectis.PLG.Tasks", "Reflectis.SDK.Tasks"),
+            new KeyValuePair<string, string>("Reflectis.PLG.TasksReflectis", "Reflectis.CreatorKit.Worlds.Tasks"),
+            new KeyValuePair<string, string>("Reflectis.SDK.CreatorKit", "Reflectis.CreatorKit.Worlds.VisualScripting"),
+            new KeyValuePair<string, string>("Reflectis.SDK.InteractionNew", "Reflectis.CreatorKit.Worlds.VisualScripting"),
+            new KeyValuePair<string, string>("GenericInteract", "VisualScriptingInteract"),
+        };
+
+        private readonly List<string> extensions = new()
+        {
+            ".unity", ".prefab", ".asset"
+        };
+
+        [MenuItem("Reflectis/CK upgrade window")]
+        public static void ShowWindow()
+        {
+            //Show existing window instance. If one doesn't exist, make one.
+            GetWindow(typeof(CreatorKitUpgradeWindow));
+        }
+
+        private void OnGUI()
+        {
+            GUIStyle style = new(EditorStyles.label)
+            {
+                richText = true,
+            };
+            if (GUILayout.Button("Migrate Reflectis version 2024.9.x -> 2025.1.0"))
+            {
+                try
+                {
+                    List<string> filePaths = new();
+                    foreach (var extension in extensions)
+                    {
+                        filePaths.AddRange(Directory.GetFiles("Assets", $"*{extension}*", SearchOption.AllDirectories));
+                    }
+
+                    foreach (string file in filePaths)
+                    {
+                        // Leggi tutto il contenuto del file
+                        string fileContent = File.ReadAllText(file);
+
+                        foreach (KeyValuePair<string, string> stringToFind in stringsToReplace)
+                        {
+                            fileContent = fileContent.Replace(stringToFind.Key, stringToFind.Value);
+                            Debug.Log($"Replaced {stringToFind.Key} -> {stringToFind.Value} in: {file}");
+                        }
+                        File.WriteAllText(file, fileContent);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+
+            if (GUILayout.Button("Migrate Reflectis version 2025.1.x -> 2025.2.0"))
+            {
+                ReplaceInteractablePlaceholder();
+            }
+        }
+
+        private void ReplaceInteractablePlaceholder()
+        {
+            // Trova tutte le scene nel progetto
+            string[] scenePaths = AssetDatabase.FindAssets("t:Scene");
+            foreach (string scenePathGuid in scenePaths)
+            {
+                string scenePath = AssetDatabase.GUIDToAssetPath(scenePathGuid);
+                UnityEditor.SceneManagement.EditorSceneManager.OpenScene(scenePath, UnityEditor.SceneManagement.OpenSceneMode.Single);
+
+                // Trova e sostituisci i componenti nella scena
+                ReplaceComponentsInScene();
+                UnityEditor.SceneManagement.EditorSceneManager.SaveScene(UnityEditor.SceneManagement.EditorSceneManager.GetActiveScene());
+            }
+
+            // Trova e sostituisci i componenti nei prefabs
+            string[] prefabPaths = AssetDatabase.FindAssets("t:Prefab");
+            foreach (string prefabPathGuid in prefabPaths)
+            {
+                string prefabPath = AssetDatabase.GUIDToAssetPath(prefabPathGuid);
+                GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+                ReplaceComponentsInPrefab(prefab);
+                PrefabUtility.SavePrefabAsset(prefab);
+            }
+
+            EditorUtility.DisplayDialog("Success", "Components replaced successfully!", "OK");
+        }
+
+        private void ReplaceComponentsInScene()
+        {
+            GameObject[] gameObjects = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
+            foreach (GameObject gameObject in gameObjects)
+            {
+                ReplaceComponentRecursive(gameObject);
+            }
+        }
+
+        private void ReplaceComponentsInPrefab(GameObject prefab)
+        {
+            ReplaceComponentRecursive(prefab);
+        }
+
+        private void ReplaceComponentRecursive(GameObject gameObject)
+        {
+            InteractablePlaceholder[] interactables = gameObject.GetComponents<InteractablePlaceholder>();
+            foreach (var interactable in interactables)
+            {
+                if (interactable != null)
+                {
+                    ReplaceInteractablePlaceholder(interactable);
+                }
+            }
+
+            foreach (Transform child in gameObject.transform)
+            {
+                ReplaceComponentRecursive(child.gameObject);
+            }
+        }
+
+        private void ReplaceInteractablePlaceholder(InteractablePlaceholder interactable)
+        {
+            Debug.Log($"Replacing component in {interactable.gameObject.name}", interactable.gameObject);
+            UnityEditor.Undo.RecordObject(interactable.gameObject, "Replace Component");
+
+            InteractionPlaceholder interactionPlaceholder = interactable.gameObject.AddComponent<InteractionPlaceholder>();
+
+            interactionPlaceholder.LockHoverDuringInteraction = interactable.LockHoverDuringInteraction;
+            interactionPlaceholder.InteractionColliders = interactable.InteractionColliders;
+            EditorUtility.SetDirty(interactionPlaceholder.gameObject);
+
+            if (interactable.InteractionModes.HasFlag(Core.Interaction.IInteractable.EInteractableType.ContextualMenuInteractable))
+            {
+                ContextualMenuPlaceholder contextualMenuPlaceholder = interactable.gameObject.AddComponent<ContextualMenuPlaceholder>();
+                contextualMenuPlaceholder.ContextualMenuOptions = interactable.ContextualMenuOptions;
+                EditorUtility.SetDirty(contextualMenuPlaceholder.gameObject);
+            }
+
+            if (interactable.InteractionModes.HasFlag(Core.Interaction.IInteractable.EInteractableType.Manipulable))
+            {
+                ManipulablePlaceholder manipulablePlaceholder = interactable.gameObject.AddComponent<ManipulablePlaceholder>();
+                manipulablePlaceholder.ManipulationMode = interactable.ManipulationMode;
+                manipulablePlaceholder.DynamicAttach = interactable.DynamicAttach;
+                manipulablePlaceholder.AdjustRotationOnRelease = interactable.AdjustRotationOnRelease;
+                manipulablePlaceholder.MouseLookAtCamera = interactable.MouseLookAtCamera;
+                manipulablePlaceholder.RealignAxisX = interactable.RealignAxisX;
+                manipulablePlaceholder.RealignAxisY = interactable.RealignAxisY;
+                manipulablePlaceholder.RealignAxisZ = interactable.RealignAxisZ;
+                manipulablePlaceholder.RealignDurationTimeInSeconds = interactable.RealignDurationTimeInSeconds;
+                manipulablePlaceholder.VrInteraction = interactable.VRInteraction;
+                manipulablePlaceholder.AttachTransform = interactable.AttachTransform;
+                EditorUtility.SetDirty(manipulablePlaceholder.gameObject);
+            }
+
+            if (!interactable.InteractionModes.HasFlag(Core.Interaction.IInteractable.EInteractableType.VisualScriptingInteractable))
+            {
+                VisualScriptingInteractablePlaceholder vsPlaceholder = interactable.gameObject.AddComponent<VisualScriptingInteractablePlaceholder>();
+                vsPlaceholder.DesktopAllowedStates = interactable.DesktopAllowedStates;
+                vsPlaceholder.VRAllowedStates = interactable.VRAllowedStates;
+                vsPlaceholder.InteractionsScriptMachine = interactable.InteractionsScriptMachine;
+                vsPlaceholder.VrVisualScriptingInteraction = interactable.VrVisualScriptingInteraction;
+                EditorUtility.SetDirty(vsPlaceholder.gameObject);
+            }
+
+            EditorUtility.SetDirty(interactionPlaceholder.gameObject);
+
+            DestroyImmediate(interactable);
+        }
+    }
+}
