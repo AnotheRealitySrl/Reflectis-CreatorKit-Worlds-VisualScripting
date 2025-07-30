aaa0595877d20290268a14b711daa17afada3e64
Raffaele Tolusso <raffaele.tolusso@gmail.com>
2025-07-30 12:16:45 +0200

    Repristinated load default env node

--- Runtime/Scripts/Nodes/Unit/LoadDefaultEventNode.cs
+++ Runtime/Scripts/Nodes/Unit/LoadDefaultEventNode.cs
@@ -1,22 +1,21 @@
+using Reflectis.CreatorKit.Worlds.Core.ApplicationManagement;
 using Reflectis.SDK.Core.VisualScripting;
 
 using System.Threading.Tasks;
 
 using Unity.VisualScripting;
-using UnityEngine;
 
 namespace Reflectis.CreatorKit.Worlds.VisualScripting
 {
-    [UnitTitle("Reflectis Platform: Load Default Event")]
+    [UnitTitle("Reflectis Platform: Load Lobby")]
     [UnitSurtitle("Platform")]
-    [UnitShortTitle("Load Default Event")]
+    [UnitShortTitle("Load Lobby")]
     [UnitCategory("Reflectis\\Flow")]
     public class LoadDefaultEventNode : AwaitableUnit
     {
         protected override async Task AwaitableAction(Flow flow)
         {
-            Debug.LogWarning($"[LoadDefaultEventNode] Default events are deprecated. The execution of this node will be ignored.");
-            //await IReflectisApplicationManager.Instance.LoadDefaultEvent();
+            await IReflectisApplicationManager.Instance.LoadLobby();
         }
     }
 }
