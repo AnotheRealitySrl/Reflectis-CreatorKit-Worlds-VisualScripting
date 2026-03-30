b5b056dc3ddd5670ce5d003d6b70b219d88db5fa
Francesco Weikmann <fweikmann@gmail.com>
2026-03-30 12:29:42 +0200

    add freeCameraPanNode

---
+++ Runtime/Scripts/Nodes/Unit/FreeCameraPanNode.cs
@@ -0,0 +1,49 @@
+using Reflectis.SDK.Core.SystemFramework;
+using Reflectis.SDK.Core.CharacterController;
+using Reflectis.SDK.Core.VisualScripting;
+
+using System.Threading.Tasks;
+
+using Unity.VisualScripting;
+
+using UnityEngine;
+
+namespace Reflectis.CreatorKit.Worlds.VisualScripting
+{
+    [UnitTitle("Reflectis Character: FreeCameraPan")]
+    [UnitSurtitle("Character")]
+    [UnitShortTitle("FreeCameraPan")]
+    [UnitCategory("Reflectis\\Flow")]
+    public class FreeCameraPanNode : AwaitableUnit
+    {
+        [NullMeansSelf]
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ValueInput TargetTransform { get; private set; }
+
+        [NullMeansSelf]
+        public ValueInput MaxZoom { get; private set; }
+
+        [NullMeansSelf]
+        public ValueInput MaxYRotation { get; private set; }
+
+        [NullMeansSelf]
+        public ValueInput MinYRotation { get; private set; }
+
+        protected override void Definition()
+        {
+            TargetTransform = ValueInput<Transform>(nameof(TargetTransform));
+            MaxZoom = ValueInput<float>(nameof(MaxZoom), 0.3f);
+            MaxYRotation = ValueInput<float>(nameof(MaxYRotation), 45f);
+            MinYRotation = ValueInput<float>(nameof(MinYRotation), -45f);
+
+            base.Definition();
+        }
+
+        protected override async Task AwaitableAction(Flow flow)
+        {
+            await SM.GetSystem<ICharacterControllerSystem>().GoToInteractState(flow.GetValue<Transform>(TargetTransform), flow.GetValue<float>(MaxZoom),
+                flow.GetValue<float>(MaxYRotation), flow.GetValue<float>(MinYRotation), true);
+        }
+    }
+}
