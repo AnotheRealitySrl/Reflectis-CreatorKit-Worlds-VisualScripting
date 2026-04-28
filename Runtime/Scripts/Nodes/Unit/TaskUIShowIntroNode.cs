92331abd862980d0c54567f26b43616bba4428a7
Francesco Weikmann <fweikmann@gmail.com>
2026-04-28 12:06:43 +0200

    add task introduction node

---
+++ Runtime/Scripts/Nodes/Unit/TaskUIShowIntroNode.cs
@@ -0,0 +1,46 @@
+﻿using Reflectis.CreatorKit.Worlds.Placeholders;
+
+using Unity.VisualScripting;
+
+namespace Reflectis.CreatorKit.Worlds.VisualScripting
+{
+    [UnitTitle("Reflectis Task: Show Introduction")]
+    [UnitSurtitle("Tasks")]
+    [UnitShortTitle("Show introduction")]
+    [UnitCategory("Reflectis\\Flow")]
+    public class TaskUIShowIntroNode : Unit
+    {
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlInput InputTrigger { get; private set; }
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlOutput OutputTrigger { get; private set; }
+
+        [NullMeansSelf]
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ValueInput TaskUIPlaceholder { get; private set; }
+
+        [NullMeansSelf]
+        [DoNotSerialize]
+        public ValueInput ShowUI { get; private set; }
+
+        protected override void Definition()
+        {
+            TaskUIPlaceholder = ValueInput<TaskUIPlaceholder>(nameof(TaskUIPlaceholder), null).NullMeansSelf();
+            ShowUI = ValueInput<bool>(nameof(ShowUI), false);
+
+            InputTrigger = ControlInput(nameof(InputTrigger), Output);
+            OutputTrigger = ControlOutput(nameof(OutputTrigger));
+
+            Succession(InputTrigger, OutputTrigger);
+        }
+
+        private ControlOutput Output(Flow flow)
+        {
+            flow.GetValue<TaskUIPlaceholder>(TaskUIPlaceholder).DisplayIntroduction(flow.GetValue<bool>(ShowUI));
+            return OutputTrigger;
+        }
+    }
+}
