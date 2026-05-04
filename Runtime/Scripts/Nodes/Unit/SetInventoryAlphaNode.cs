d8c3b0b581dd48e50b1563692df97bbda367aa17
Francesco Weikmann <fweikmann@gmail.com>
2026-05-04 17:24:00 +0200

    create setAlphaInventory node

---
+++ Runtime/Scripts/Nodes/Unit/SetInventoryAlphaNode.cs
@@ -0,0 +1,41 @@
+using Reflectis.SDK.Core.SystemFramework;
+using Unity.VisualScripting;
+using Reflectis.SDK.Inventory;
+
+namespace Reflectis.CreatorKit.Worlds.VisualScripting
+{
+    [UnitTitle("Reflectis Inventory: Set Alpha")]
+    [UnitSurtitle("SetAlpha")]
+    [UnitShortTitle("Set Alpha")]
+    [UnitCategory("Reflectis\\Flow")]
+    public class SetInventoryAlphaNode : Unit
+    {
+        [DoNotSerialize]
+        public ValueInput Alpha { get; private set; }
+
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlInput InputTrigger { get; private set; }
+
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlOutput OutputTrigger { get; private set; }
+
+        protected override void Definition()
+        {
+            Alpha = ValueInput<float>(nameof(Alpha));
+
+            InputTrigger = ControlInput(nameof(InputTrigger), (f) =>
+            {
+                SM.GetSystem<IInventorySystem>().SetInventoryAlpha(f.GetValue<float>(Alpha));
+
+                return OutputTrigger;
+            });
+
+            OutputTrigger = ControlOutput(nameof(OutputTrigger));
+
+            Succession(InputTrigger, OutputTrigger);
+        }
+
+    }
+}
